932e6a8aa9f8cfafceeb8e7c94e1a4ca4f50adcd
Diego <73961367+deviego@users.noreply.github.com>
2023-03-11 15:11:03 -0300

    lib api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const isProduction = process.env.NODE_ENV === 'production';
-const base = 'http://localhost:3000/api';
+const base = isProduction
+  ? 'https://snowye.dev/api'
+  : 'http://localhost:3000/api';
 
 export const api = axios.create({
   baseURL: base
