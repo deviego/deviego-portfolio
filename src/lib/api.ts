148132ca382424368e69d8176a9a81fb066c30fd
deviego <deviego4@gmail.com>
2023-03-21 14:30:49 -0300

    teste api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const isProduction = process.env.NODE_ENV === 'production';
 const base = isProduction
-  ? 'https://deviego.vercel.app/email'
-  : 'http://localhost:3000/email';
+  ? 'https://deviego.vercel.app/api'
+  : 'http://localhost:3000/api';
 
 export const api = axios.create({
   baseURL: base
