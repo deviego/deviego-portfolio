b53f3496a3246ed3d8f40d6ed67575ef16120b82
deviego <deviego4@gmail.com>
2023-05-17 20:05:01 -0300

    atualization

--- src/data/career.ts
+++ src/data/career.ts
@@ -5,7 +5,7 @@ export const jobs: Job[] = [
     jobTitle: 'Software engineer freelancer',
     company: 'Boticário',
     companyUrl: 'https://www.instagram.com/boticario_rj_bonsucesso/',
-    startDate: '2022-08-01',
+    startDate: '2022-07-01',
     endDate: '2022-11-01',
     location: 'Rio de Janeiro, RJ'
   },
@@ -15,6 +15,14 @@ export const jobs: Job[] = [
     companyUrl: 'https://deviego.github.io/BeautySalon/',
     startDate: '2022-05-02',
     endDate: '2022-05-13',
+    location: 'Rio de Janeiro, RJ'
+  },
+  {
+    jobTitle: 'Front-End Developer freelancer',
+    company: 'Serpens',
+    companyUrl: 'https://talkhub.com.br/',
+    startDate: '2023-02-10',
+    endDate: '',
     location: 'Rio de Janeiro, RJ'
   }
 ];
